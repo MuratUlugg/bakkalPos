487b8d4ed559b77c357cb259028037860ebc7e99
muratulugg <murat.ulug@kerzz.com.tr>
2020-12-14 16:00:31 +0300

    page - laoyut  - nav  added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { MainNavComponent } from './nav/main-nav/main-nav.component';
+import { AdminNavComponent } from './nav/admin-nav/admin-nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SaleComponent } from './pages/sale/sale.component';
+import { InvoiceComponent } from './pages/invoice/invoice.component';
+import { AccountComponent } from './pages/account/account.component';
+import { ReportComponent } from './pages/report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    AdminLayoutComponent,
+    MainNavComponent,
+    AdminNavComponent,
+    HomeComponent,
+    SaleComponent,
+    InvoiceComponent,
+    AccountComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
