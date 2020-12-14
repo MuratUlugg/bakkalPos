db8f9a8b99881a06504e620969c7d709f2906f93
muratulugg <murat.ulug@kerzz.com.tr>
2020-12-14 16:20:58 +0300

    temel routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,49 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { AccountComponent } from './pages/account/account.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InvoiceComponent } from './pages/invoice/invoice.component';
+import { ReportComponent } from './pages/report/report.component';
+import { SaleComponent } from './pages/sale/sale.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //www.bakkalPos.com/
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        //www.bakkalPos.com/
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        //www.bakkalPos.com/sale
+        path: 'sale',
+        component: SaleComponent,
+      },
+      {
+        //www.bakkalPos.com/invoice
+        path: 'invoice',
+        component: InvoiceComponent,
+      },
+      {
+        //www.bakkalPos.com/account
+        path: 'account',
+        component: AccountComponent,
+      },
+      {
+        //www.bakkalPos.com/report
+        path: 'report',
+        component: ReportComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
